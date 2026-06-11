5069ce5f8f8ef9ece294dc34c5d8a16bb861140b
l <maelapouge@gmail.com>
2026-06-11 18:53:17 +0200

    pulled from area/backend-api-db to link the db to log, sign, token and user files

--- backend/src/middlewares/authToken.js
+++ backend/src/middlewares/authToken.js
@@ -1,24 +1,24 @@
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
-// to delete and replace with data base
-const users = [{"name":"Mee", "password":"$2b$10$hOp9KiGy.aWwqNNemmzpROAjPUsabTFD018PMMvUBKrBTcz8LV7IK"}]
-
-
 async function authToken(req, res, next) {
-	const authHeader = req.headers['authorization']
-	const token = authHeader && authHeader.split(' ')[1]
-	if (!token)
-		return res.sendStatus(401)
+	try {
+		const authHeader = req.headers['authorization'];
+		const token = authHeader && authHeader.split(' ')[1];
+		
+		if (!token)
+			return res.sendStatus(401);
 
-	jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, user) => {
-		if (err)
-			return res.sendStatus(403) // no longer valid token
-		req.user = user
-		next()
-	})
+		jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, user) => {
+			if (err)
+				return res.sendStatus(403);
+			
+			req.user = user;
+			next();
+		});
+	} catch (err) {
+		console.error("Auth middleware error: ", err);
+		res.status(500).json({ error: 'Server error' });
+	}
 }
-
 
 module.exports = authToken;
