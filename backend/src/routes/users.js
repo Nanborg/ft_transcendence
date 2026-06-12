4c41ae116cbdddcdf2bff6ca5b5b8ef6c51f88d5
yaoberso <yannoberson@hotmail.com>
2026-06-12 14:09:11 +0200

    feat(backend): Database setup (PostgreSQL + Prisma) and Users route (#29)
    * chore: init Node project and install Express
    
    * chore: add .gitignore for node_modules
    
    * feat: add Makefile and validate base server
    
    * chore: install Prisma ORM and update .gitignore
    
    * chore: prisma init and migration
    
    * feat: link express to prisma and add user POST route
    
    * chore: add .env.example and automate prisma migration on startup
    
    * refactor(backend): clean routing architecture
    
    Moved Prisma logic to db.js, cleaned server.js, and exposed port 3000 for local testing.
    
    * style: remove trailing whitespace in schema.prisma
    
    ---------
    
    Co-authored-by: Yann Oberson <yann@MacBook-Air-de-Yann.local>

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -3,8 +3,23 @@ const router = express.Router();
 
 const OAuth = require("../middlewares/OAuth");
 
+const prisma = require('../db');
+
 router.get("/me", OAuth, (req, res) => {
 	res.json(req.user);
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { email, username } = req.body;
+        const newUser = await prisma.user.create({
+            data: { email: email, username: username },
+        });
+        res.status(201).json({ message: "Joueur créé avec succès !", user: newUser });
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: "Impossible de créer le joueur" });
+    }
 });
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,30 +5,22 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const port = Number(process.env.BACKEND_PORT || 3000);
-const frontendUrl = process.env.FRONTEND_URL || "https://localhost";
 
 const app = express();
 app.use(express.json());
 
 const server = http.createServer(app);
-
 const io = new Server(server);
-
 require("./socket/socketHandler")(io);
-
-
 
 const healthRoutes = require("./routes/health");
 const privateRoutes = require("./routes/private");
-const usersRoutes = require("./routes/users")
+const usersRoutes = require("./routes/users");
 
 app.use("/health", healthRoutes);
-
 app.use("/private", privateRoutes);
-
 app.use("/users", usersRoutes);
 
-
 server.listen(port, "0.0.0.0", () => {
-	console.log(`backend listening on ${port}`);
+    console.log(`backend listening on port ${port}`);
 });
