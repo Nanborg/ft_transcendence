965734ed8266193e56afa2a8ffbace246c69cf05
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-07-02 14:08:35 +0200

    feat(front): add leaderboard mock data

---
+++ frontend/src/features/leaderboard/mockLeaderboard.js
@@ -0,0 +1,5 @@
+export const mockLeaderboard = [
+    { rank: 1, username: 'user1', wins: 12, losses: 3},
+    { rank: 2, username: 'user2', wins: 5, losses: 2},
+    { rank: 3, username: 'user3', wins: 1, losses: 5},
+];
