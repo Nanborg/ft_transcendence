1a99d09008589ba417d681e02174dfb2c82298c1
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-07-13 06:16:33 +0200

    fix(game-engine): initialize inherited velocity fields

--- game_engine/src/AbstractMovingEntity.cpp
+++ game_engine/src/AbstractMovingEntity.cpp
@@ -1,9 +1,11 @@
 #include "AbstractMovingEntity.hpp"
 
 AbstractMovingEntity::AbstractMovingEntity( unsigned int type, int size, int posX, int posY, int velX, int velY, int health, int passableHitBox ):
-	AbstractEntity(type, size, posX, posY, health, passableHitBox),
-	_velX(velX),
-	_velY(velY) {}
+	AbstractEntity(type, size, posX, posY, health, passableHitBox)
+{
+	_velX = velX;
+	_velY = velY;
+}
 
 AbstractMovingEntity::~AbstractMovingEntity( void ) {}
 
