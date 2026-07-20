e22befe33aa73fdc125889971bfc145cd2f89faf
l <maelapouge@gmail.com>
2026-07-20 14:59:54 +0200

    added token creation when registering a new account/ just login in, see .docREADME.md for branch current state informations

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -4,6 +4,11 @@ const { OAuth } = require("../middlewares/OAuth");
 router.use(express.json());
 const prisma = require('../db');
 const bcrypt = require("bcrypt");
+const {generateAccessToken} = require('../middlewares/OAuth');
+const jwt = require("jsonwebtoken");
+
+require("../middlewares/OAuth");
+
 
 
 
@@ -43,96 +48,122 @@ router.get("/42", (req, res) => {
 });
 
 router.get("/42/callback", async (req, res) => {
-	const code = req.query.code;
-
-	if (!code) {
-		return res.status(400).send("Missing code");
-	}
-
-	// exchange code for a token
-	const response = await fetch("https://api.intra.42.fr/oauth/token", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			grant_type: "authorization_code",
-			client_id: process.env.FT_CLIENT_ID,
-			client_secret: process.env.FT_CLIENT_SECRET,
-			code: code,
-			redirect_uri: "https://localhost/login/42/callback",
-		}),
-	});
-
-	const data = await response.json();
-	console.log(data);
-	if (!data.access_token) {
-		return res.status(401).json(data);
-	}
-
-	// call /v2/me to get user data
-	const infos_response = await fetch("https://api.intra.42.fr/v2/me", {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": "Bearer " + data.access_token
-		}
-	});
-
-	const user_data = await infos_response.json();
-	console.log(user_data);
-
-
-	// Chercher/créer l'utilisateur
-
-	user = await prisma.user.findUnique({
-	where: { fortyTwoId: user_data.id }
-	});
-
-	if (!user)
-	{
-		user = await prisma.user.findUnique({
-		where: { email: user_data.email }
-		});
-	}
-	if (!user)
-	{
-		try {
-			const hashedPassword = await bcrypt.hash("placeholder", 10) //???????????????????????????????????????
-
-			const created = await prisma.user.create({
-				data: {
-					username: user_data.login,
-					email: user_data.email,
-					password: hashedPassword
-				}
-			});
-
-			res.status(201).json({
-				message: 'Register success',
-				userId: created.id,
-				username: created.username
-			});
-		}
-		catch (err){
-				if (err.code === 'P2002') {
-					return res.status(400).json({ error: 'Email or username already exists' });
-			}
+	try{
+		const code = req.query.code;
 	
-			console.error("Auth error: ", err);
-			res.status(500).send()
+		if (!code) {
+			return res.status(400).send("Missing code");
+		}
+	
+		// exchange code for a token
+		const response = await fetch("https://api.intra.42.fr/oauth/token", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				grant_type: "authorization_code",
+				client_id: process.env.FT_CLIENT_ID,
+				client_secret: process.env.FT_CLIENT_SECRET,
+				code: code,
+				redirect_uri: "https://localhost/login/42/callback",
+			}),
+		});
+	
+		const data = await response.json();
+		console.log(data);
+		if (!data.access_token) {
+			return res.status(401).json(data);
+		}
+	
+		// call /v2/me to get user data
+		const infos_response = await fetch("https://api.intra.42.fr/v2/me", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": "Bearer " + data.access_token
+			}
+		});
+	
+		const user_data = await infos_response.json();
+		console.log(user_data);
+	
+	
+		// Chercher/créer l'utilisateur
+	
+		user = await prisma.user.findUnique({
+		where: { fortyTwoId: user_data.id }
+		});
+	
+		if (!user)
+		{
+			user = await prisma.user.findUnique({
+			where: { email: user_data.email }
+			});
+		}
+		if (!user)
+		{
+			try {
+				const hashedPassword = await bcrypt.hash("placeholder", 10) //???????????????????????????????????????
+	
+				const created = await prisma.user.create({
+					data: {
+						username: user_data.login,
+						email: user_data.email,
+						password: hashedPassword
+					}
+				});
+	
+	
+				const u = {
+					id: created.id,
+					username: created.username
+				}
+				const accessToken = generateAccessToken(u)
+				const refreshToken = jwt.sign(u, process.env.REFRESH_SECRET_TOKEN)
+				const expiresAt = new Date()
+				expiresAt.setDate(expiresAt.getDate() + 7)
+				await prisma.refreshToken.create({
+					data: { token: refreshToken, userId: created.id, expiresAt: expiresAt }
+				});
+		
+		
+				res.status(201).json({ message: "Register and Connection success", username: created.username, accessToken: accessToken, refreshToken: refreshToken })
+			}
+			catch (err){
+					if (err.code === 'P2002') {
+						return res.status(400).json({ error: 'Email or username already exists' });
+				}
+		
+				console.error("Auth error: ", err);
+				res.status(500).send()
+			}
+		}
+		else
+		{
+			const u = {
+				id: user.id,
+				username: user.username
+			}
+			const accessToken = generateAccessToken(u)
+			const refreshToken = jwt.sign(u, process.env.REFRESH_SECRET_TOKEN)
+			const expiresAt = new Date()
+			expiresAt.setDate(expiresAt.getDate() + 7)
+			await prisma.refreshToken.create({
+				data: { token: refreshToken, userId: user.id, expiresAt: expiresAt }
+			});
+	
+	
+			res.json({ message: "Connection success", accessToken: accessToken, refreshToken: refreshToken })
+	
 		}
 	}
-	else
-		res.status(302).json({
-			message: 'already existing ',
-			username: user.username
-		});
+	catch (err)
+	{
+	    console.error(err);
+		console.error(err.cause);
+	}
 		
-
-
-
-	// Generate JWTs
 });
 
 
