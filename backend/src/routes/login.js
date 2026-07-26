65aad9078917a173fa122c8936adf2523b9972e3
l <maelapouge@gmail.com>
2026-07-26 14:23:35 +0200

    corrected wrong redirrection (just added /api ...) and corrected the reused body

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -41,7 +41,7 @@ router.get("/42", (req, res) => {
 	const url =
 		"https://api.intra.42.fr/oauth/authorize" +
 		"?client_id=" + process.env.FT_CLIENT_ID +
-		"&redirect_uri=" + encodeURIComponent("https://localhost/login/42/callback") +
+		"&redirect_uri=" + encodeURIComponent("https://localhost/api/login/42/callback") +
 		"&response_type=code";
 
 	return res.redirect(url);
@@ -93,7 +93,7 @@ router.get("/42/callback", async (req, res) => {
 				client_id: process.env.FT_CLIENT_ID,
 				client_secret: process.env.FT_CLIENT_SECRET,
 				code: code,
-				redirect_uri: "https://localhost/login/42/callback",
+				redirect_uri: "https://localhost/api/login/42/callback",
 			}),
 		});
 		let body = await response.text();
@@ -102,7 +102,7 @@ router.get("/42/callback", async (req, res) => {
 			return res.status(response.status).send(body);
 		}
 
-		const data = await response.json();
+		const data = JSON.parse(body);
 		if (!data.access_token) {
 			return res.status(401).json(data);
 		}
@@ -120,7 +120,7 @@ router.get("/42/callback", async (req, res) => {
 			return res.status(infos_response.status).send(body);
 		}
 
-		const userData = await infos_response.json();
+		const userData = JSON.parse(body);
 		if (!userData.id || !userData.email)
 			return res.sendStatus(500);
 
