5bf240e552eca98fe4808a3f5bb7ee500d5109a3
Yann Oberson <yann@MacBook-Air-de-Yann.local>
2026-07-28 15:21:23 +0200

    feat: update game stats persistence, schema and test script

---
+++ backend/prisma/migrations/20260728092952_add_detailed_game_stats/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - Added the required column `abandoned` to the `GameRun` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lost` to the `GameRun` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `damageDealt` to the `PlayerRunStats` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `damageReceived` to the `PlayerRunStats` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `deaths` to the `PlayerRunStats` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `upgrade1` to the `PlayerRunStats` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `upgrade2` to the `PlayerRunStats` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `upgrade3` to the `PlayerRunStats` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "GameRun" ADD COLUMN     "abandoned" BOOLEAN NOT NULL,
+ADD COLUMN     "lost" BOOLEAN NOT NULL;
+
+-- AlterTable
+ALTER TABLE "PlayerRunStats" ADD COLUMN     "damageDealt" INTEGER NOT NULL,
+ADD COLUMN     "damageReceived" INTEGER NOT NULL,
+ADD COLUMN     "deaths" INTEGER NOT NULL,
+ADD COLUMN     "upgrade1" INTEGER NOT NULL,
+ADD COLUMN     "upgrade2" INTEGER NOT NULL,
+ADD COLUMN     "upgrade3" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "RefreshToken" ALTER COLUMN "expiresAt" DROP DEFAULT;
