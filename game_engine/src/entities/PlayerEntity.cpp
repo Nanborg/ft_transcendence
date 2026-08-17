9d1d8e0ed333feb0713c69b5fcaceb76eaf2de56
princiamf2 <mmfuri6@gmail.com>
2026-08-17 23:54:35 +0200

    feat: activate held player shield

--- game_engine/src/entities/PlayerEntity.cpp
+++ game_engine/src/entities/PlayerEntity.cpp
@@ -3,18 +3,23 @@
 const float	PlayerEntity::_slashDist = 0.5f;
 const int	PlayerEntity::_meleeCooldownTicks = 10;
 const int	PlayerEntity::_rangedCooldownTicks = 20;
+const int	PlayerEntity::_shieldCooldownTicks = 30;
+const int	PlayerEntity::_shieldBaseHealth = 30;
+const int	PlayerEntity::_shieldHealthPerLevel = 10;
 
 PlayerEntity::PlayerEntity( int playerId, int posX, int posY, int velX, int velY ):
 	AbstractMovingEntity(EntityTypes::PLAYERENTITY, g_game->getScale(), posX, posY, velX, velY, 10, false),
 	_playerId(playerId),
 	_receivedInput(true),
-	_curAction(PlayerActions::NOACTION) {
+	_curAction(PlayerActions::NOACTION),
+	_shieldEntityId(-1) {
 			std::cout << "new player (id " << _playerId << ")\n";
 		}
 
 PlayerEntity::~PlayerEntity( void ) {}
 
 bool	PlayerEntity::tick( void ) {
+	_check_shield_state();
 	// TODO(neon-05): Finalize player state for game_state serialization:
 	// HP, score, XP/level, alive/dead state, arena bounds, and input effects.
 	if (_receivedInput) {
@@ -44,12 +49,24 @@ void PlayerEntity::movementInput( int velX, int velY ) {
 	}
 }
 
+void PlayerEntity::_check_shield_state(void)
+{
+	if (_curAction != PlayerActions::SHIELD || _shieldEntityId < 0)
+		return;
+	if (g_game->getEntityIterator(_shieldEntityId) != g_game->getEntityList().end())
+		return;
+	_finish_shield();
+}
+
 void PlayerEntity::playerAction( const json& in ) {
 	PlayerActions action = in["action"];
 	switch (action)
 	{
 	case PlayerActions::NOACTION:
-		_curAction = PlayerActions::NOACTION;
+		if (_curAction == PlayerActions::SHIELD)
+			_finish_shield();
+		else
+			_curAction = PlayerActions::NOACTION;
 		break;
 
 	case PlayerActions::MELEEATT:
@@ -62,6 +79,7 @@ void PlayerEntity::playerAction( const json& in ) {
 
 	case PlayerActions::SHIELD:
 		_action_shield(in);
+		break;
 
 	default:
 		break;
@@ -123,10 +141,50 @@ void	PlayerEntity::_action_range( const json& in ) {
 	_curAction = PlayerActions::NOACTION;
 }
 
-void	PlayerEntity::_action_shield( const json& in ) {
-	if (_curAction != PlayerActions::NOACTION)
-		return;
-	_curAction = PlayerActions::SHIELD;
-	// add cooldown
+void PlayerEntity::_action_shield(const json& in)
+{
+    (void)in;
 
+    if (_curAction != PlayerActions::NOACTION)
+        return;
+    GameEngine::PlayerData* playerData =
+        g_game->getPlayerData(_playerId);
+    if (!playerData ||
+        playerData->cooldowns.shield > 0)
+        return;
+    const int shieldHealth =
+        _shieldBaseHealth +
+        playerData->upgrades.shield *
+            _shieldHealthPerLevel;
+    LaserShieldEntity* shield =
+        new LaserShieldEntity(
+            _posX,
+            _posY,
+            shieldHealth,
+            _id,
+            0
+        );
+    _shieldEntityId = shield->getId();
+    _curAction = PlayerActions::SHIELD;
+    g_game->spawnEntity(shield);
+}
+
+void PlayerEntity::_finish_shield(void)
+{
+    if (_shieldEntityId >= 0)
+    {
+        GameEngine::entityList_t::iterator shieldIt =
+            g_game->getEntityIterator(_shieldEntityId);
+        if (shieldIt != g_game->getEntityList().end())
+            g_game->deleteEntity(shieldIt);
+    }
+    _shieldEntityId = -1;
+    _curAction = PlayerActions::NOACTION;
+    GameEngine::PlayerData* playerData =
+        g_game->getPlayerData(_playerId);
+    if (!playerData)
+        return;
+    playerData->cooldowns.shield =
+        _shieldCooldownTicks;
+    g_game->sendPlayerStateUpdate(*playerData);
 }
