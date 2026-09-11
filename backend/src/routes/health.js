ed02e94e21faac7089e488c1c796f65e03e6dd2d
Loufoko <maelapougewerner@gmail.com>
2026-09-11 15:23:08 +0200

    Update health.js so it has the req parameter

--- backend/src/routes/health.js
+++ backend/src/routes/health.js
@@ -17,6 +17,6 @@ const router = express.Router();
 
 //		{"status":"ok"}
 
-router.get("/", (res) => {res.json({ status: "ok" });});
+router.get("/", (_, res) => {res.json({ status: "ok" });});
 
 module.exports = router;
