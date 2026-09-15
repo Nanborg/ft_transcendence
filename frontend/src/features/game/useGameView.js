e329e0f0656bc3d480130d186765a4c8209b59d3
princiamf2 <mmfuri6@gmail.com>
2026-09-15 23:10:25 +0200

    typo correction

--- frontend/src/features/game/useGameView.js
+++ frontend/src/features/game/useGameView.js
@@ -2,10 +2,10 @@ import { useMemo, useSyncExternalStore } from "react";
 
 export function useGameView(gameStore, playerId)
 {
-    const getSnashot = useMemo(() => gameStore.createPlayerView(playerId), [gameStore, playerId]);
+    const getSnapshot = useMemo(() => gameStore.createPlayerView(playerId), [gameStore, playerId]);
     return useSyncExternalStore(
         gameStore.subscribe,
-        getSnashot,
-        getSnashot
+        getSnapshot,
+        getSnapshot
     );
 }
